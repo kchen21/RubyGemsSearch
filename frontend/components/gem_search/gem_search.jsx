820c9d760342a2da63268303588201d26faefcfb
Kenneth Chen <kennethchendbgt@gmail.com>
2017-06-30 15:08:27 -0400

    Update GemSearch component

--- frontend/components/gem_search/gem_search.jsx
+++ frontend/components/gem_search/gem_search.jsx
@@ -7,13 +7,47 @@ class GemSearch extends React.Component {
       keyword: "",
       searchResults: {}
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  return (
-    <div>
-      
-    </div>
-  );
+  handleChange(propertyName) {
+    return (e) => {
+      return this.setState({ [propertyName]: e.currentTarget.value });
+    };
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.fetchGemSearchResults(this.state.keyword).then(() => {
+      this.setState({ searchResults: this.props.searchResults });
+    });
+  }
+
+  render() {
+    const gemList = [];
+
+    return (
+      <div>
+        <h1>Search Gems</h1>
+        <form className="gem-search-form" onSubmit={ this.handleSubmit }>
+          <input
+            type="text"
+            onChange={ this.handleChange("keyword") }
+            value={ this.state.keyword }
+            />
+          <input type="submit" style="position: absolute; left: -9999px"/>
+          <img src={ windows.assets.magnifying_glass } />
+        </form>
+        <ul>
+          { gemList }
+        </ul>
+        { this.state.searchResults }
+      </div>
+    );
+  }
+
 }
 
 export default GemSearch;
