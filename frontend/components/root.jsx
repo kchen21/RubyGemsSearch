56b3292067dcd39f4939032818195e5aaf328af0
Kenneth Chen <kennethchendbgt@gmail.com>
2017-06-30 15:13:33 -0400

    Create React route for search

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route, hashHistory, Redirect } from 'react-router-dom';
 import Home from './home/home';
+import GemSearchContainer from './gem_search/gem_search_container';
 
 const Root = ({ store }) => {
   return (
     <Provider store={ store }>
       <BrowserRouter history={ hashHistory }>
+        <redirect from="/" to="search" />
         <Route path="/" component={ Home }>
+          <Route path="search" component={ GemSearchContainer } />
         </Route>
       </BrowserRouter>
     </Provider>
