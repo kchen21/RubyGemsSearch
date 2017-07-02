61fc02743e4184056ee000ad5ab864fa70a23a73
Kenneth Chen <kennethchendbgt@gmail.com>
2017-07-02 13:36:13 -0400

    Update Root component

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route, browserHistory, Redirect } from 'react-router-dom';
+import { HashRouter as Router, Route, hashHistory, Redirect } from 'react-router-dom';
 import Home from './home/home';
 import GemSearchContainer from './gem_search/gem_search_container';
 
 const Root = ({ store }) => {
   return (
     <Provider store={ store }>
-      <Router history={ browserHistory }>
+      <Router history={ hashHistory }>
         <Home>
           <Route path='/search' component={ GemSearchContainer } />
         </Home>
