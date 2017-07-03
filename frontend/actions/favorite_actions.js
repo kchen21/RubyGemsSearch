41747caf24ad0b2d6927d0d4b955055b8a37349e
Kenneth Chen <kennethchendbgt@gmail.com>
2017-07-03 14:16:10 -0400

    Set up favorite actions

---
+++ frontend/actions/favorite_actions.js
@@ -0,0 +1,38 @@
+import * as FavoriteAPIUtil from '../util/favorite_util';
+
+export const RECEIVE_FAVORITES = 'RECEIVE_FAVORITES';
+
+export const receiveFavorites = (favorites) => ({
+  type: RECEIVE_FAVORITES,
+  favorites
+});
+
+export const fetchFavorites = () = {
+  return (dispatch) => {
+    const success = (favorites) => {
+      return dispatch(receiveFavorites(favorites));
+    };
+
+    return FavoriteAPIUtil.fetchFavorites().then(success);
+  };
+};
+
+export const createFavorite = (favorite) => {
+  return (dispatch) => {
+    const success = (favorites) => {
+      return dispatch(receiveFavorites(favorites));
+    };
+
+    return FavoriteAPIUtil.createFavorite(favorite).then(success);
+  };
+};
+
+export const deleteFavorite = (id) => {
+  return (dispatch) => {
+    const success = (favorites) => {
+      return dispatch(receiveFavorites(favorites));
+    };
+
+    return FavoriteAPIUtil.deleteFavorite(id).then(success);
+  };
+};
